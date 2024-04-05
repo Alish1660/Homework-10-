5a28e57512033c0e4994cd441f1d28c6a923fbb8
Alish1660 <Sardor9894>
2024-04-05 11:58:27 -1200

    second

--- main.js
+++ main.js
@@ -76,3 +76,15 @@
 //   console.log(sumOfDigitsArray(...numbers));
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //   11-misol ///////////////////////////////////////////////////////////////////////////////////////////////////////
+// const sumDigits = (num) => {
+//     let sum = 0;
+//     while (num) {
+//       sum += num % 10;
+//       num = Math.floor(num / 10);
+//     }
+//     return sum;
+//   };
+//   const userInput = prompt("Istalgan sonni kiriting:");
+//   const number = parseInt(userInput);
+//   const result = sumDigits(number);
+//   console.log(`Kiritilgan sonning raqamlari yig'indisi: ${result}`);
